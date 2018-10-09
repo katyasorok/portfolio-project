34246f00085065724b611a513f2645b6f544998f
Katya So <katyasorok@msn.com>
2018-10-09 17:30:03 -0400

    jobs up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    """docstring for ."""
+    image = models.ImageField(upload_to ='images/')
+    summary = models.CharField(max_length = 200)
