9852c995a8e74f51381a1b0eebf88ab2da72e9a0
Katya So <katyasorok@msn.com>
2018-10-09 20:47:24 -0400

    running jobs from database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    """docstring for ."""
+    title = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to ='images/')
